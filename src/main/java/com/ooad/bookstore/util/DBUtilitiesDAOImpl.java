fe674093cbd2bf88af487b24d2df5df76edde37d
Vik <vik@indigoconsulting.com>
2017-06-24 15:04:05 -0500

    Made the constructor private

--- src/main/java/com/ooad/bookstore/util/DBUtilitiesDAOImpl.java
+++ src/main/java/com/ooad/bookstore/util/DBUtilitiesDAOImpl.java
@@ -2,9 +2,12 @@ package com.ooad.bookstore.util;
 
 import java.io.FileNotFoundException;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
 
+import com.ooad.bookstore.model.BookDetails;
 import com.ooad.bookstore.model.Customer;
 
 public class DBUtilitiesDAOImpl {
@@ -13,6 +16,7 @@ public class DBUtilitiesDAOImpl {
 	public static final int SQL_INTEGRITY_CONSTRAIN_VIOLATION_EXCEPTION = 2;
 	public static final int SQL_EXCEPTION = 3;
 	private static DBUtilitiesDAOImpl dbUtilitiesDAOImpl = null;
+	private static final String DATABASE_NAME = "bookstore";
 
 	public static DBUtilitiesDAOImpl getInstance() {
 		if (dbUtilitiesDAOImpl == null) {
@@ -21,7 +25,7 @@ public class DBUtilitiesDAOImpl {
 		return dbUtilitiesDAOImpl;
 	}
 
-	public DBUtilitiesDAOImpl() {
+	private DBUtilitiesDAOImpl() {
 
 	}
 
@@ -29,7 +33,7 @@ public class DBUtilitiesDAOImpl {
 
 		String CustomerRegistration = "INSERT INTO customerAccount VALUES " + "(?,?,?,?,?,?,?,?,?,?,?,?,?);";
 		try {
-			PreparedStatement preparedStatement = DBConnection.getConnection("bookstore")
+			PreparedStatement preparedStatement = DBConnection.getConnection(DATABASE_NAME)
 					.prepareStatement(CustomerRegistration);
 			preparedStatement.setString(1, getCustomerHelper().getUserID());
 			preparedStatement.setString(2, getCustomerHelper().getCustomerFirstName());
@@ -57,7 +61,35 @@ public class DBUtilitiesDAOImpl {
 		}
 	}
 
+	public ArrayList<BookDetails> getBookDetails() {
+
+		String getBookDetails = "SELECT bookName, bookType, bookAvailability, bookPrice FROM bookDetails";
+		PreparedStatement pst;
+		try {
+			pst = DBConnection.getConnection(DATABASE_NAME).prepareStatement(getBookDetails);
+			ResultSet resultSet = pst.executeQuery();
+			ArrayList<BookDetails> arrayList = new ArrayList<BookDetails>();
+			while (resultSet.next()) {
+				BookDetails bookDetails = new BookDetails();
+				bookDetails.setBook(resultSet.getString("bookName"));
+				bookDetails.setType(resultSet.getString("bookType"));
+				bookDetails.setAvailability(resultSet.getString("bookAvailability"));
+				bookDetails.setPrice(resultSet.getString("bookPrice"));
+				arrayList.add(bookDetails);
+			}
+			return arrayList;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 	private Customer getCustomerHelper() {
 		return Customer.getInstance();
 	}
+
 }
