de3f00d4739c49289dd249fe69d8f1d824158824
Vik <vik@indigoconsulting.com>
2017-06-24 15:05:52 -0500

    POJO class for book details

---
+++ src/main/java/com/ooad/bookstore/model/BookDetails.java
@@ -0,0 +1,42 @@
+package com.ooad.bookstore.model;
+
+public class BookDetails {
+
+	private String book;
+	private String type;
+	private String availability;
+	private String price;
+
+	public String getBook() {
+		return book;
+	}
+
+	public void setBook(String book) {
+		this.book = book;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getAvailability() {
+		return availability;
+	}
+
+	public void setAvailability(String availability) {
+		this.availability = availability;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+}
